d08d61dbf5d4a50f0696fb77c562ede577c907f0
sunidhi23 <dalmia.sunidhi23@gmail.com>
2019-08-08 12:56:56 +0530

    first

---
+++ fullstack/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import HeadingPage from './componentNew/HeadingPage';
+import Content from './componentNew/Content';
+
+
+function App() {
+  return (
+    <div>
+      <HeadingPage/>
+      <br/>
+      <Content/>
+      
+
+    </div>
+  );
+}
+
+export default App;
